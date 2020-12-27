7d1a16285ac7e50ded1d1d235cd0a24633144f9c
Vaishakh Kallattil <vkallattil@gmail.com>
2020-12-27 18:35:34 -0500

    rearranged some folders

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <>
+      <header className='header'>
+        <a href='/' className='site-name-link'>
+          Quizzes
+        </a>
+      </header>
+    </>
+  );
+}
+
+export default App;
