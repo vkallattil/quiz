7deb5e3e3d0570f2b6d37c99d863639ecce08be4
Vaishakh Kallattil <vkallattil@gmail.com>
2020-12-27 18:44:39 -0500

    Added react-router and initial paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
-import { Fragment } from "react";
+import React from "react";
+import Home from "./Home/Home";
+import Profile from "./Profile/Profile";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "../styles/App.css";
 
 function App() {
   return (
-    <>
+    <Router>
       <header className='header'>
-        <a href='/' className='site-name-link'>
-          Quizzes
-        </a>
+        <nav>
+          <Link to='/' className='site-name-link'>
+            Quizzes
+          </Link>
+          <Link to='/profile' className='nav-link'>
+            Profile
+          </Link>
+        </nav>
       </header>
-    </>
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/profile'>
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
